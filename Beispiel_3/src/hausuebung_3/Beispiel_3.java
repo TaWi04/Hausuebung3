c5e5ff8e8d457df7edee6a92ed0ba7cc21c84947
unknown <tamarawimmer04@gmail.com>
2020-10-08 21:19:14 +0200

    implemented "is0", "isNull" and "isShortWord" + finished Beispiel_3

--- Beispiel_3/src/hausuebung_3/Beispiel_3.java
+++ Beispiel_3/src/hausuebung_3/Beispiel_3.java
@@ -25,12 +25,29 @@ public class Beispiel_3 {
         // Calling Predicate method 
         System.out.println(lesserthan.test(10));
        // int number;
-       int auswahl = -12;
-        Predicate<Integer> isEven = number -> {return (number % 2) == 0;};
+       int auswahl = 18;
+       String temp = null;
+        Predicate<Integer> isEven = number -> {return (number % 2) == 0;};      //isEven
         System.out.println("Is " + auswahl + " even?" +"\n"+ isEven.test(auswahl));
-        IntPredicate isPositive = number -> {return number > 0;};
+        
+        Predicate<Integer> isPositive = number -> {return number > 0;};         //isPositive
         System.out.println("Is " + auswahl + " positive?\n" + isPositive.test(auswahl));
         
+        Predicate<Integer> is0 = number -> {return number == 0;};               //isZero
+        System.out.println("Is " + auswahl + " zero?" +"\n"+ is0.test(auswahl));
+        
+        Predicate isNull = number -> {return number == null;};                  //isNull
+        System.out.println("Is " + temp + " null?\n" + isNull.test(temp));
+        
+        String shortWord = "Hey";
+        String longWord = "Halleluja";
+        Predicate<String> isShortWord = word -> {return word.length() < 4;};    //isShortWord
+        System.out.println("Is " + shortWord + " a short word?\n" + isShortWord.test(shortWord));
+        System.out.println("Is " + longWord + "  a short word?\n" + isShortWord.test(longWord));
+        
+        System.out.println("Is " + auswahl + " positive and even?\n" + isPositive.and(isEven).test(auswahl)); //mix with positve and even + and
+        
+        System.out.println("Is " + auswahl + " positive and odd?\n" + isPositive.and(isEven.negate()).test(auswahl)); //mix with positve and even + and + negate
     }
     
 }
